55b59c019b15c42964e8e28d4969cb65ee083855
Faraaz Ahmed Jabbar <41717039+faraazjabbar@users.noreply.github.com>
2026-04-21 12:14:35 +0530

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        let encoded = "";
+        for(let str of strs) {
+            encoded += str.length+"#"+str;
+        }
+        return encoded;
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let decoded = [];
+        let i =0;
+        while(i< str.length) {
+            let j = i;
+            while(str[j] !== '#') {
+                j++;
+            }
+            let length = parseInt(str.substring(i,j));
+            i = j+1;
+            j = i + length;
+            decoded.push(str.substring(i,j));
+            i = j;
+        }
+        
+        return decoded
+    }
+}
