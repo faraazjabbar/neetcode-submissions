d94a9cf4fcd2f7e42c7328570d8e5a6b7fd0c579
Faraaz Ahmed Jabbar <41717039+faraazjabbar@users.noreply.github.com>
2026-04-29 13:27:43 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.js
@@ -0,0 +1,38 @@
+class MinStack {
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    /**
+     * @param {number} val
+     * @return {void}
+     */
+    push(val) {
+        this.stack.push(val)
+        let pushVal = this.minStack[this.minStack.length-1] < val ? this.minStack[this.minStack.length-1] : val;
+        this.minStack.push(pushVal);
+    }
+
+    /**
+     * @return {void}
+     */
+    pop() {
+        this.stack.pop()
+        this.minStack.pop()
+    }
+
+    /**
+     * @return {number}
+     */
+    top() {
+        return this.stack[this.stack.length-1]
+    }
+
+    /**
+     * @return {number}
+     */
+    getMin() {
+        return Math.min(...this.stack)
+    }
+}
